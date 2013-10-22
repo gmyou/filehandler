bd0a561200c5b7fff6db818432e099868f16dd25
Jack You <gmyou71@gmail.com>
2013-10-22 16:52:32 +0900

    add mode

--- fileRename.py
+++ fileRename.py
@@ -17,20 +17,24 @@ def findMusic(dir):
     f = file("fileRename.sh", "a")
     for fname in dirList:
         if fname.endswith("mp3"):
+            print "\t"+fname
+            """
             refname = findRename(fname)
             print("\t"+str(refname))
+
             if (fname != refname):
                 print "\t"+"\t"+dir+fname
                 print "\t"+"\t"+dir+refname
                 cmd = "mv " + dir+fname + " " + dir+refname
                 f.write(cmd+"\n")
+            """
     f.close()
             
 def findCD():
     dirList=os.listdir(path)
     for fname in dirList:
         if fname.startswith("CD"):
-            print path+fname
+            print fname
             findMusic(path+fname)
 
 

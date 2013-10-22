4d6be4bf55ae26707bb90680e8e0bbd695accb9d
Jack You <gmyou71@gmail.com>
2013-10-22 16:01:37 +0900

    rename list.py

---
+++ fileRename.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+import os, shutil
+
+path="/Volumes/DATA/Dropbox/Audio/FavorateSong/"  # insert the path to the directory of interest here
+
+def findRename(source):
+    destination = source.replace(" ", "")
+    destination = destination.replace("`", "")
+    destination = destination.replace("'", "")
+    destination = destination.replace("(", "_")
+    destination = destination.replace(")", "_")
+    return destination
+
+def findMusic(dir):
+    dirList=os.listdir(dir)
+    
+    f = file("fileRename.sh", "a")
+    for fname in dirList:
+        if fname.endswith("mp3"):
+            refname = findRename(fname)
+            print("\t"+str(refname))
+            if (fname != refname):
+                print "\t"+"\t"+dir+fname
+                print "\t"+"\t"+dir+refname
+                cmd = "mv " + dir+fname + " " + dir+refname
+                f.write(cmd+"\n")
+    f.close()
+            
+def findCD():
+    dirList=os.listdir(path)
+    for fname in dirList:
+        if fname.startswith("CD"):
+            print path+fname
+            findMusic(path+fname)
+
+
+f = file("fileRename.sh", "w")
+f.write("")
+f.close()
+findCD()
--- list.py
+++
@@ -1,6 +0,0 @@
-import os
-
-path="/Volumes/SSD/Audio"  # insert the path to the directory of interest here
-dirList=os.listdir(path)
-for fname in dirList:
-    print fname
